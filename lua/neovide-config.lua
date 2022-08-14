ea8d199c2ba4d25bb478d08b40adcff762f6a8ec
ry@pop_os <ryan.brilliant.nirwana@gmail.com>
2022-08-14 10:17:19 +0700

    config neovide

---
+++ lua/neovide-config.lua
@@ -0,0 +1,10 @@
+if vim.fn.exists('g:neovide') then
+  vim.g.neovide_transparency=0.8
+  vim.g.neovide_floating_blur_amount_x = 2.0
+  vim.g.neovide_floating_blur_amount_y = 2.0
+  vim.g.neovide_fullscreen=true
+  vim.g.neovide_confirm_quit=true
+  vim.g.neovide_cursor_animation_length=0.13
+  vim.g.neovide_cursor_vfx_mode="railgun"
+end
+
