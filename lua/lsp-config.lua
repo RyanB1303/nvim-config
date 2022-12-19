b9b3212d75bd205e66da7a8fe9c196abcf5fc19e
Ryan B <ryan.brilliant.nirwana@gmail.com>
2022-12-19 19:18:40 +0700

    some depreceation warning

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -36,9 +36,9 @@ local lsp_flags = {
 	-- This is the default in Nvim 0.7+
 	debounce_text_changes = 150,
 }
-local capabilities = {
-	require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-}
+-- local capabilities = {
+-- 	require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+-- }
 --lua
 require("lspconfig")["sumneko_lua"].setup({
 	on_attach = on_attach,
